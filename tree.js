21513bc562331ee6fe11419d720dbf287eacadb9
Maria Juravic <mariajuravic@yahoo.com>
2023-02-08 11:22:24 -0800

    complete tree.js

--- tree.js
+++ tree.js
@@ -11,21 +11,35 @@ class TreeNode {
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
-
+    let sum = this.val;
+    for(let child of this.children) {
+      sum += child.sumValues();
+    }
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
+    let count = this.val % 2 === 0 ? 1 : 0;
 
+    for(let child of this.children) {
+      count += child.countEvens();
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
   numGreater(lowerBound){
+    let count = this.val > lowerBound ? 1 : 0;
 
+    for(let child of this.children) {
+      count += child.numGreater(lowerBound);
+    }
+    return count;
   }
 }
 
@@ -36,18 +50,28 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-
+    if(this.root === null) {
+      return 0;
+    }
+    return this.root.sumValues();
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
+    if(this.root === null) {
+      return 0;
+    }
+    return this.root.countEvens();
 
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
   numGreater(lowerBound) {
-
+    if(this.root === null) {
+      return 0;
+    }
+    return this.root.numGreater(lowerBound);
   }
 }
 
